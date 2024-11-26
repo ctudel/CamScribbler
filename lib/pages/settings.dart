3b6645c547cd31fd1e373e0505dea39ea59ea264
ctudel <christudela@u.boisestate.edu>
2024-11-26 16:29:29 -0700

    Modify and implement settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:provider/provider.dart';
+
+import 'package:cam_scribbler/providers/providers.dart';
 
 class Settings extends StatefulWidget {
   const Settings({
@@ -12,28 +16,38 @@ class Settings extends StatefulWidget {
 
 class _SettingsState extends State<Settings> {
   bool _autoSave = false;
-  bool _grid = true;
 
   @override
   Widget build(BuildContext context) {
+    final bool isLight =
+        AdaptiveTheme.of(context).mode == AdaptiveThemeMode.light;
+    final bool isGrid = context.watch<SettingsProvider>().isGrid;
+
     return Padding(
       padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
       child: Padding(
         padding: const EdgeInsets.only(top: 8.0, left: 30.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
                 const Text('Appearance'),
-                Switch(
-                  value:
-                      AdaptiveTheme.of(context).mode == AdaptiveThemeMode.light,
-                  activeColor: Colors.amber,
-                  onChanged: (bool value) {
-                    if (value)
-                      AdaptiveTheme.of(context).setLight();
-                    else
-                      AdaptiveTheme.of(context).setDark();
+                IconButton(
+                  icon: isLight
+                      ? PhosphorIcon(PhosphorIcons.sunHorizon())
+                      : PhosphorIcon(PhosphorIcons.moonStars()),
+                  style: IconButton.styleFrom(
+                    backgroundColor: (isLight) ? Colors.amber : Colors.black26,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (isLight)
+                        AdaptiveTheme.of(context).setDark();
+                      else
+                        AdaptiveTheme.of(context).setLight();
+                      print(AdaptiveTheme.of(context).mode);
+                    });
                   },
                 ),
               ],
@@ -41,7 +55,8 @@ class _SettingsState extends State<Settings> {
             const SizedBox(height: 20),
             Row(
               children: [
-                const Text('Auto Save'),
+                const Text('Auto Save Drawings'),
+                const SizedBox(width: 10),
                 Switch(
                   value: _autoSave,
                   activeColor: Colors.amber,
@@ -54,18 +69,38 @@ class _SettingsState extends State<Settings> {
               ],
             ),
             const SizedBox(height: 20),
+            const Text('Drawing Gallery View'),
             Row(
               children: [
-                const Text('Grid View'),
-                Switch(
-                  value: _grid,
-                  activeColor: Colors.amber,
-                  onChanged: (bool value) {
+                IconButton(
+                  style: IconButton.styleFrom(
+                    backgroundColor: (isGrid) ? Colors.amber : Colors.black12,
+                    foregroundColor: (isGrid)
+                        ? Colors.black
+                        : Theme.of(context).colorScheme.onSurface,
+                  ),
+                  icon: PhosphorIcon(PhosphorIcons.gridFour()),
+                  onPressed: () {
                     setState(() {
-                      _grid = value;
+                      context.read<SettingsProvider>().setGrid();
                     });
                   },
-                )
+                ),
+                const SizedBox(width: 10),
+                IconButton(
+                  style: IconButton.styleFrom(
+                    backgroundColor: (!isGrid) ? Colors.amber : Colors.black12,
+                    foregroundColor: (!isGrid)
+                        ? Colors.black
+                        : Theme.of(context).colorScheme.onSurface,
+                  ),
+                  icon: PhosphorIcon(PhosphorIcons.images()),
+                  onPressed: () {
+                    setState(() {
+                      context.read<SettingsProvider>().setCarousel();
+                    });
+                  },
+                ),
               ],
             ),
           ],
