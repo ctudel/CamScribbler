a7d549ffc47aae556baf7726b130ff08e0dcf89d
ctudel <christudela@u.boisestate.edu>
2024-11-29 00:16:29 -0700

    Fixed drawable parsing bug

--- lib/models/drawable_convert.dart
+++ lib/models/drawable_convert.dart
@@ -3,39 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_painter/flutter_painter.dart';
 
 String drawablesToJson(PainterController controller) {
-  List<Map<String, dynamic>> listOfMaps = []; // stores converted drawables
+  // Extract drawables if grouped
+  List<Drawable> drawables = _extractDrawables(controller.drawables);
+  List<Map<String, dynamic>> listOfMaps =
+      <Map<String, dynamic>>[]; // stores converted drawables
 
   // Make all drawables into JSON format
-  for (final Drawable drawable in controller.drawables) {
-    if (drawable is FreeStyleDrawable) {
-      final FreeStyleDrawable fsDrawable = drawable;
-      listOfMaps.add({
-        'type': 'FreeStyleDrawable',
-        'stroke': fsDrawable.strokeWidth,
-        'color': fsDrawable.color.value,
-        'path':
-            fsDrawable.path // Create a list of maps containing offset values
-                .map((offset) => {
-                      'dx': offset.dx,
-                      'dy': offset.dy,
-                    })
-                .toList(),
-      });
-    }
-
-    if (drawable is EraseDrawable) {
-      final EraseDrawable erasable = drawable;
-      listOfMaps.add({
-        'type': 'EraseDrawable',
-        'stroke': erasable.strokeWidth,
-        'path': erasable.path
-            .map((offset) => {
-                  'dx': offset.dx,
-                  'dy': offset.dy,
-                })
-            .toList(),
-      });
-    }
+  for (final Drawable drawable in drawables) {
+    listOfMaps.add(_parseDrawables(drawable));
   }
 
   return jsonEncode(listOfMaps);
@@ -64,9 +39,7 @@ List<Drawable> jsonToDrawables(String jsonStr) {
         color: Color(d['color']),
         path: list,
       ));
-    }
-
-    if (d['type'] == 'EraseDrawable') {
+    } else if (d['type'] == 'EraseDrawable') {
       drawables.add(EraseDrawable(
         strokeWidth: d['stroke'],
         path: list,
@@ -75,4 +48,48 @@ List<Drawable> jsonToDrawables(String jsonStr) {
   }
 
   return drawables;
+}
+
+Map<String, dynamic> _parseDrawables(Drawable drawable) {
+  if (drawable is FreeStyleDrawable) {
+    final FreeStyleDrawable fsDrawable = drawable;
+    return {
+      'type': 'FreeStyleDrawable',
+      'stroke': fsDrawable.strokeWidth,
+      'color': fsDrawable.color.value,
+      'path': fsDrawable.path // Create a list of maps containing offset values
+          .map((offset) => {
+                'dx': offset.dx,
+                'dy': offset.dy,
+              })
+          .toList(),
+    };
+  } else if (drawable is EraseDrawable) {
+    final EraseDrawable erasable = drawable;
+    return {
+      'type': 'EraseDrawable',
+      'stroke': erasable.strokeWidth,
+      'path': erasable.path
+          .map((offset) => {
+                'dx': offset.dx,
+                'dy': offset.dy,
+              })
+          .toList(),
+    };
+  } else
+    return {};
+}
+
+List<Drawable> _extractDrawables(List<Drawable> drawables) {
+  List<Drawable> list = <Drawable>[];
+
+  for (final Drawable drawable in drawables) {
+    if (drawable.runtimeType.toString() == 'GroupedDrawable') {
+      list.addAll(_extractDrawables((drawable as dynamic).drawables));
+    } else if (drawable is FreeStyleDrawable || drawable is EraseDrawable) {
+      list.add(drawable);
+    }
+  }
+
+  return list;
 }
